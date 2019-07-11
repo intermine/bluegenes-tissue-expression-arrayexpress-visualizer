4f895f5ac8ff12192dfd49b0dec0f1446da7c7a9
akshat <akshatbhargavadeepa@gmail.com>
2019-07-11 18:07:11 +0530

    removed unused block in query test

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,41 +1,38 @@
 const imjs = require('imjs');
 const queryExpData = require('../src/queryData').default;
 
-describe('Query', () => {
+describe('query', () => {
 	const mockData = {
 		gene: 'KRAG',
 		serviceUrl: 'http://www.humanmine.org/human'
 	};
+	test('should return a promise that resolves with correct result json', () => {
+		const queryRes = queryExpData(mockData.gene, mockData.serviceUrl, imjs);
 
-	describe('query', () => {
-		test('should return a promise that resolves with correct result json', () => {
-			const queryRes = queryExpData(mockData.gene, mockData.serviceUrl, imjs);
+		return queryRes.then(res => {
+			expect(res).toHaveProperty('atlasExpression');
+			expect(res.atlasExpression).toBeInstanceOf(Array);
 
-			return queryRes.then(res => {
-				expect(res).toHaveProperty('atlasExpression');
-				expect(res.atlasExpression).toBeInstanceOf(Array);
+			const firstVal = res.atlasExpression[0];
+			expect(firstVal).toHaveProperty('condition');
+			expect(firstVal).toHaveProperty('expression');
+			expect(firstVal).toHaveProperty('tStatistic');
+			expect(firstVal).toHaveProperty('pValue');
+			expect(firstVal).toHaveProperty('objectId');
+		});
+	});
 
-				const firstVal = res.atlasExpression[0];
-				expect(firstVal).toHaveProperty('condition');
-				expect(firstVal).toHaveProperty('expression');
-				expect(firstVal).toHaveProperty('tStatistic');
-				expect(firstVal).toHaveProperty('pValue');
-				expect(firstVal).toHaveProperty('objectId');
-			});
+	test('should throw error when data corresponding to organism is not found', () => {
+		const dataWithInvalidGeneId = Object.assign({}, mockData, {
+			gene: 'KAG' // some wrong gene
 		});
 
-		test('should throw error when data corresponding to organism is not found', () => {
-			const dataWithInvalidGeneId = Object.assign({}, mockData, {
-				gene: 'KAG' // some wrong gene
-			});
+		const queryRes = queryExpData(
+			dataWithInvalidGeneId.gene,
+			dataWithInvalidGeneId.serviceUrl,
+			imjs
+		);
 
-			const queryRes = queryExpData(
-				dataWithInvalidGeneId.gene,
-				dataWithInvalidGeneId.serviceUrl,
-				imjs
-			);
-
-			return expect(queryRes).rejects.toBe('No data found!');
-		});
+		return expect(queryRes).rejects.toBe('No data found!');
 	});
 });
