4440a50f9ed284f48e84944703ce8e05b7083aca
akshat <akshatbhargavadeepa@gmail.com>
2019-07-11 13:52:57 +0530

    added test for query

---
+++ tests/query.test.js
@@ -0,0 +1,41 @@
+const imjs = require('imjs');
+const queryExpData = require('../src/queryData').default;
+
+describe('Query', () => {
+	const mockData = {
+		gene: 'KRAG',
+		serviceUrl: 'http://www.humanmine.org/human'
+	};
+
+	describe('query', () => {
+		test('should return a promise that resolves with correct result json', () => {
+			const queryRes = queryExpData(mockData.gene, mockData.serviceUrl, imjs);
+
+			return queryRes.then(res => {
+				expect(res).toHaveProperty('atlasExpression');
+				expect(res.atlasExpression).toBeInstanceOf(Array);
+
+				const firstVal = res.atlasExpression[0];
+				expect(firstVal).toHaveProperty('condition');
+				expect(firstVal).toHaveProperty('expression');
+				expect(firstVal).toHaveProperty('tStatistic');
+				expect(firstVal).toHaveProperty('pValue');
+				expect(firstVal).toHaveProperty('objectId');
+			});
+		});
+
+		test('should throw error when data corresponding to organism is not found', () => {
+			const dataWithInvalidGeneId = Object.assign({}, mockData, {
+				gene: 'KAG' // some wrong gene
+			});
+
+			const queryRes = queryExpData(
+				dataWithInvalidGeneId.gene,
+				dataWithInvalidGeneId.serviceUrl,
+				imjs
+			);
+
+			return expect(queryRes).rejects.toBe('No data found!');
+		});
+	});
+});
