8ad81d4d560f676b4828a5a408b23c616779ca2e
akshat <akshatbhargavadeepa@gmail.com>
2019-07-11 13:15:04 +0530

    integrated chart in RootContainer

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import ExpressionChart from './chart';
+import queryData from './queryData';
+import getChartData from './chartData';
 
 class RootContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { chartData: null };
+	}
+
+	componentDidMount() {
+		const {
+			entity: { value },
+			serviceUrl
+		} = this.props;
+
+		queryData(value, serviceUrl).then(res => {
+			const { atlasExpression } = res;
+			this.setState({ chartData: getChartData(atlasExpression) });
+		});
+	}
+
 	render() {
 		return (
 			<div className="rootContainer">
-				<h1>Your Data Viz Here</h1>
+				{this.state.chartData && (
+					<ExpressionChart chartData={this.state.chartData} />
+				)}
 			</div>
 		);
 	}
