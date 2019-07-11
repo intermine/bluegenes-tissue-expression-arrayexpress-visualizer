f76ecd2110c9a3687dd5b1d7c49f1d18da385913
akshat <akshatbhargavadeepa@gmail.com>
2019-07-11 13:21:33 +0530

    added loading in the viz

---
+++ src/loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Loading() {
+	return (
+		<div className="loading">
+			<div>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	);
+}
+
+export default Loading;
